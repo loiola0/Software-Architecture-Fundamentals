8f9967f837174df3b6f40186a64770f9092e7016
loiola0 <victorloiola@alu.ufc.br>
2022-05-27 15:57:43 -0300

    upload NerdStore project

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,94 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public Guid CategoriaId { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Descricao { get; private set; }
+
+        public bool Ativo { get; private set; }
+
+        public decimal Valor { get; private set; }
+
+        public DateTime DataCadastro { get; private set; }
+
+        public string Imagem { get; private set; }
+
+        public int QuantidadeEstoque { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public Produto(string nome,string descricao,bool ativo,Guid categoriaId,decimal valor,DateTime dataCadastro,string imagem)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+        }
+
+        //ad hoc setters
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void DebitarProduto(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            if(quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void validar()
+        {
+            
+        }
+    }
+
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+
+        public int Codigo { get; private set; }
+
+        public Categoria(string nome,int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+        public string Amor = "Eu amo para conhecer"; 
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
